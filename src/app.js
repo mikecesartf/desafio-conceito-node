9eae0b33eace28b5c3dfaad4c2e236e743dd0594
Mike Cesar <mikecesartf@hotmail.com>
2020-07-27 17:04:49 -0300

    iniciando projeto do github

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,81 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title: 'Desafio Node.js', url: 'http://github.com/...', techs: ["Node.js", "..."], likes: 0 };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoriesIndex = repositories.findIndex(repositories => repositories.id == id);
+
+  if (repositoriesIndex < 0) {
+    return response.status(400).json({ error: ' Repositories not Found.' })
+  }
+
+  const repositories = {
+    title,
+    url,
+    techs,
+  };
+
+  repositories[repositoriesIndex] = repositories;
+
+  return response.json(repositories);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoriesIndex = repositories.findIndex(repositories => repositories.id == id);
+
+  if (repositoriesIndex < 0) {
+    return response.status(400).json({ error: ' Repositories not Found.' })
+  }
+
+  repositories.splice(repositoriesIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+  const { id } = request.params;
+
+  const repositoriesIndex = repositories.findIndex(repositories => repositories.id == id);
+
+  if (repositoriesIndex < 0) {
+    return response.status(400).json({ error: ' Repositories not Found.' })
+  }
+
+  const { likes: qtd, title, url, techs } = repositories[repositoriesIndex]
+  const likes = qtd + 1
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  };
+
+  repositories[repositoriesIndex] = repository;
+
+  return response.json(repository);
+
+}
+);
 
 module.exports = app;
